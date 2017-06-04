7314060d7468f98fc716bb96387c1bc4f79c71eb
Nallely House <nallelyht@gmail.com>
2017-06-04 12:23:11 -0500

    maquetacion

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-$(document).ready(function(){
+var cargarPagina = function(){
 	// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
 	$('.modal').modal();
 	$(".button-collapse").sideNav();
+	
+	
 
-});
+};
+
+$(document).ready(cargarPagina);
